13db56ababbd745574938fcac4abb1a24a3278e3
jhuan280 <92900160+jhuan280@users.noreply.github.com>
2024-10-08 18:22:08 -0700

    Step 1
    Created a button

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,25 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing game \n by Jackie Huang";
+//title
+const gameName = "Jackie's amazing game";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+//------------------button-------------------------
+
+// Create a new button element and set the text
+const button = document.createElement('button');
+button.textContent = '🥡';
+
+// Add a click event listener to the button
+button.addEventListener('click', () => {
+    console.log('Button was clicked!');
+    alert('This button will eventually do something cool!');
+});
+
+// Append the button 
+app.appendChild(button);
